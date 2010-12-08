069ca06bc15d2289b56bce42503132a2f7517b13
hlach <hlach@nbty.com>
2010-12-08 18:59:01 -0500

    first exercise done

--- product/nothinbutdotnetstore/web/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/DefaultFrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web
 {
     public class DefaultFrontController : FrontController
     {
+        private CommandRegistry commandRegistry;
+
+        public DefaultFrontController(CommandRegistry commandRegistry)
+        {
+            this.commandRegistry = commandRegistry;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            RequestCommand command = commandRegistry.get_the_command_that_can_process(request);
+            command.process(request);
         }
     }
 }
