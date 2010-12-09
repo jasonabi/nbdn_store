7781cd3390bf1d480b9f6cac106bda6d11888f19
Annmarie Ziegler <annmarieziegler@nbty.com>
2010-12-08 19:39:51 -0500

    whatever

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -7,15 +7,21 @@ namespace nothinbutdotnetstore.web
     public class DefaultCommandRegistry : CommandRegistry
     {
         IEnumerable<RequestCommand> commands;
+        private MissingCommandFactory missingCommandFactory;
 
-        public DefaultCommandRegistry(IEnumerable<RequestCommand> commands)
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> commands, MissingCommandFactory factory)
         {
             this.commands = commands;
+            this.missingCommandFactory = factory;
         }
-
         public RequestCommand get_the_command_that_can_process(Request request)
         {
-            return commands.First(x => x.can_process(request));
+
+            RequestCommand command = commands.FirstOrDefault(x => x.can_process(request));
+            if (command == null)
+                return missingCommandFactory.create();
+
+            return command;
 
         }
     }
--- product/nothinbutdotnetstore/web/MissingCommandFactory.cs
+++ product/nothinbutdotnetstore/web/MissingCommandFactory.cs
@@ -4,4 +4,5 @@ namespace nothinbutdotnetstore.web
     {
         RequestCommand create();
     }
+
 }
