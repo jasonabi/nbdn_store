65bc836137f2798534ee91660c057fd2bd83bc94
unknown <JPalmer@.Enterprise.NBTY.Global>
2010-12-08 19:20:26 -0500

    asdfafd

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        private IEnumerable<RequestCommand> _commands;
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> commands)
+        {
+            _commands = commands;
+        }
+
         public RequestCommand get_the_command_that_can_process(Request request)
         {
-            throw new NotImplementedException();
+            var cmd = from c in _commands
+                      where c.can_process(request)
+                      select c;
+            return cmd.FirstOrDefault();
+
         }
     }
 }
