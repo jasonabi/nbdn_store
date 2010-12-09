a4ca166b5cf73f6b081575fb77c5759e3a5952e7
JIntardonato <jintardonato@nbty.com>
2010-12-09 15:17:04 -0500

    updates

--- product/nothinbutdotnetstore/web/application/ViewDepartmentsInADepartment.cs
+++ product/nothinbutdotnetstore/web/application/ViewDepartmentsInADepartment.cs
@@ -1,13 +1,24 @@
 using System;
+using nothinbutdotnetstore.tasks;
+using nothinbutdotnetstore.web.application.model;
 using nothinbutdotnetstore.web.infrastructure;
 
 namespace nothinbutdotnetstore.web.application
 {
     public class ViewDepartmentsInADepartment : ApplicationCommand
     {
+        private Repository _repository;
+        private ResponseEngine _responseEngine;
+
         public void process(Request request)
+        {                        
+            this._responseEngine.prepare(_repository.get_all_departments_in( request.map<Department>()));
+        }
+
+        public ViewDepartmentsInADepartment(Repository repository, ResponseEngine responseEngine)
         {
-            throw new NotImplementedException();
+            _responseEngine = responseEngine;
+            _repository = repository;
         }
     }
 }
